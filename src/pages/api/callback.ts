52cf462c8d83bbd420618f6b0ecb3a4241b290c3
Elias Meire <elias@meire.dev>
2022-04-20 16:12:33 +0200

    Move callback route

--- src/pages/api/callback.ts
+++ src/pages/api/callback.ts
@@ -2,6 +2,12 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { stringify } from 'querystring'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  const vaultUrl = new URL('https://unify.apideck.com/vault/callback')
   const query = stringify(req.query)
-  res.redirect(`https://unify.apideck.com/vault/callback?${query}`)
+
+  if (query) {
+    vaultUrl.search = query
+  }
+
+  res.redirect(vaultUrl.toString())
 }
